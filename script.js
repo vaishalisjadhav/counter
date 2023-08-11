c35ba528ae7b13f6617cadd65a43463edf91fa86
vaishalisjadhav <89831689+vaishalisjadhav@users.noreply.github.com>
2023-08-11 21:37:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const countdownDate = new Date('2023-12-31').getTime();
+
+function updateCountdown() {
+  const now = new Date().getTime();
+  const timeRemaining = countdownDate - now;
+
+  const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+  document.getElementById('days').textContent = days;
+  document.getElementById('hours').textContent = hours;
+  document.getElementById('minutes').textContent = minutes;
+  document.getElementById('seconds').textContent = seconds;
+}
+
+updateCountdown();
+setInterval(updateCountdown, 1000);
